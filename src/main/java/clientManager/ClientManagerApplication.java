2366068096c9a987904eb2b21c3b6c4dbf0a37ac
Agustin Perez Garcia <agustinperezgarciaa@gmail.com>
2020-10-26 00:08:14 -0300

    remove error swagger entry

--- src/main/java/clientManager/ClientManagerApplication.java
+++ src/main/java/clientManager/ClientManagerApplication.java
@@ -1,10 +1,14 @@
 package clientManager;
 
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * @author Agustin Perez Garcia
@@ -16,5 +20,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ClientManagerApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ClientManagerApplication.class, args);
+	}
+
+	@Bean
+	public Docket removeSwaggerErrorEntry() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.any())
+			.paths(Predicates.not(PathSelectors.regex("/error.*")))
+			.build();
 	}
 }
